7e3ed5b2d9ca08ee7d48ee5cef4d0942e6c29f9b
Kevin Miraglia <miraglia.k@gmail.com>
2018-09-05 23:47:20 +0000

    setup react and express

---
+++ dashboard.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path');
+const app = express();
+const routes = require('./controllers');
+const staticAssetsPath = path.resolve(__dirname, 'build');
+
+app.engine('html', require('ejs').renderFile);
+app.use('/', routes); //Main entry point
+app.set('views', './src')
+app.use(express.static(staticAssetsPath));
+
+
+
+
+
+app.listen(8000, () => {
+  console.log('app listening on port 8000!')
+});
